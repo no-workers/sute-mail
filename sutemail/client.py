1f86495e978d257abae5b06ce7c8922e22581ee4
Ch31212y <okaoka1567@gmail.com>
2020-06-22 01:02:37 +0900

    UID,cokkiesログインの追加、chromeドライバの自動ダウンロート

---
+++ sutemail/client.py
@@ -0,0 +1,62 @@
+import sys
+import json
+import os
+import time
+import zipfile
+import platform
+
+import urllib.request
+
+try:
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver import Chrome
+except ImportError:
+    print("we cant find selenium module\ndownloading selenium module...\n\n")
+    os.system("pip install selenium")
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver import Chrome
+
+from .const import Const
+from .auth import Auth
+
+
+class MailClient(Auth):
+    def __init__(self, user_id: str=None, passwd: str=None):
+        Auth.__init__(self)
+        if user_id and passwd:
+            self.login_with_uid_and_passwd(user_id, passwd)
+        else:
+            self.login_with_cookies()
+
+    def start_chrome_driver(self, path):
+        """
+            start chrome driver without any head and log
+        """
+        options = Options()
+        options.add_argument("--headless")
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument('--log-level=3')
+        print("starting chrome")
+        self.driver = Chrome(options=options, executable_path=path)
+
+
+def check_driver():
+    system = platform.system()
+    if os.path.isfile(Const.driver_path[system]):
+        return Const.driver_path[system]
+    elif "chromedriver" not in os.environ:
+        path = "./chromedriver.zip"
+        print("We can't find chromedriver in your environ\ninstalling chrome driver....")
+
+        urllib.request.urlretrieve(Const.driver_urls[system], path)
+
+        print(f"success download\nunpacking zip...\n")
+
+        with zipfile.ZipFile(path) as zip_file:
+            zip_file.extractall()
+
+        print("success\n")
+        os.remove(path)
+        return Const.driver_path[system]
+    else:
+        return "chromedriver"
