5f01d58c10bd2134c3f01e9332409407efdeba74
Ch31212y <okaoka1567@gmail.com>
2020-06-21 23:34:39 +0900

    configを追加

---
+++ sutemail/config.py
@@ -0,0 +1,11 @@
+
+
+class Config:
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+
+    HOST = 'https://m.kuku.lu'
+
+    INDEX = "/index.php"
+    ADDRLIST = "/index._addrlist.php"
+    NEW = "/new.php"
+    RECV_MAIL = "/smphone.app.recv.view.php"
