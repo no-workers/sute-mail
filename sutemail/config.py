7695d874ec4d893909b3f6992be3062259c542c4
Ch31212y <okaoka1567@gmail.com>
2020-06-22 02:51:04 +0900

    メールを受信するためのepヺ追加

--- sutemail/config.py
+++ sutemail/config.py
@@ -1,7 +1,7 @@
 
 
 class Config:
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_6) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.12 Safari/534.24"
 
     HOST = 'https://m.kuku.lu'
 
@@ -9,3 +9,6 @@ class Config:
     ADDRLIST = "/index._addrlist.php"
     NEW = "/new.php"
     RECV_MAIL = "/smphone.app.recv.view.php"
+    RECV = "/recv._ajax.php"
+
+    DOMINS = set()
